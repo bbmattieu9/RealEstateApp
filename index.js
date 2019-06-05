213879de4cea0868a28106d9813ce0c6f9af6fdd
Babatunde Mattieu <bbmattieu9@gmail.com>
2019-06-05 14:55:28 +0100

    creating routes

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require('express');
-
-
-
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 9001;
 
-app.get('/', (req, res) => {
-    res.send('Working!');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+  );
 });
 
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello Real Estate Lovers!</h1>");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server runs on PORT ${PORT}`);
-})
+  console.log(`Server runs on PORT ${PORT}`);
+});
